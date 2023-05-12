e39c0949b00bab2bdbc2fc5979783395df39dd40
Ahaan-Mandal <122046904+Ahaan-Mandal@users.noreply.github.com>
2023-05-12 17:27:35 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 song= ""
-
+leftWristX=0;
+rightWristY=0;
+rightWristX=0
+leftWristY=0;
 function setup()
 {
     canvas=createCanvas(600, 500);
@@ -8,8 +11,15 @@ function setup()
     video = createCapture(VIDEO);
     video.hide();
 
+    poseNet= ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+
 } 
 
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
 
 
 function draw() {
@@ -25,5 +35,20 @@ function preload(){
 function play() {
 
 song.play();
+song.setVolume(1);
+song.rate(1);
 
+}
+
+function gotPoses(results) {
+    if(results.length >  0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX+ " leftWristY = " + leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX+ " rightWristY = " + rightWristY);
+    }
 }
