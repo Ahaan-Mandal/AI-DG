7571a6bc47329597777802384b1a03c780093ed7
Ahaan-Mandal <122046904+Ahaan-Mandal@users.noreply.github.com>
2023-05-15 17:51:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ leftWristX=0;
 rightWristY=0;
 rightWristX=0
 leftWristY=0;
+scoreRightWrist=0;
+scoreLeftWrist=0;
+
 function setup()
 {
     canvas=createCanvas(600, 500);
@@ -24,6 +27,18 @@ function modelLoaded(){
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("red");
+    stroke("red");
+    if (scoreLeftWrist > 0.2) {
+
+        circle(leftWristX, leftWristY, 25);
+        InNumberleftWristY= Number(leftWristY);
+        remove_decimals= floor(InNumberleftWristY);
+        volume= remove_decimals/500;
+        document.getElementById("volume").innerHTML= "Volume = " + volume;
+        song.setVolume(volume);
+
+    }
 
 }
 
@@ -43,6 +58,10 @@ song.rate(1);
 function gotPoses(results) {
     if(results.length >  0){
         console.log(results);
+        scoreRightWrist= results[0].pose.keypoints[10].score;
+        scoreLeftWrist= results[0].pose.keypoints[9].score;
+        console.log(" scoreRightWrist= " + scoreRightWrist + " scoreLeftWrist= " + scoreLeftWrist);
+        
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX+ " leftWristY = " + leftWristY);
